53cfaa7821a70d46651cfe7d31e73de59ab203ce
David <David@4a3fcead-100c-0410-8434-c0d6b4fee585>
2006-02-24 17:39:38 +0000

    Change compress_dll so an array of search pats are used. Update file_format.txt with compress_dll info.
    git-svn-id: http://crab/source/c#@35 4a3fcead-100c-0410-8434-c0d6b4fee585

--- exe_shrink/trunk/expander_dll/main.c
+++ exe_shrink/trunk/expander_dll/main.c
@@ -6,16 +6,104 @@
     #define DLL_EXPORT
 #endif
 
+//functions
+int getSearchPaths(char *SearchPath[], int count);
+
 HGLOBAL DLL_EXPORT decompress_block(void* data, DWORD dataSize)
 {
-    HGLOBAL block;
+    char *buffer;
+    int searchCount = 3;           // number of paths in search array
+    char *searchPath[searchCount]; // array of paths to search for files
 
-    block = GlobalAlloc(GMEM_FIXED, dataSize);
-    if (block)
+    HGLOBAL block;
+    char* nameLength;
+    int* dataLength;
+    void* blockdata;
+    int i;
+    char message[100];
+
+    //fill searchPath array
+    if (getSearchPaths(searchPath, searchCount) == 0)
     {
-        CopyMemory(block, data, dataSize);
+        //error filling serachPath array
+        return;
     }
-    return block;
+    return 0;
+
+    //get intial block of memory
+    block = GlobalAlloc(GMEM_FIXED, blockSize);
+    if (block == NULL)
+    {
+        MessageBox(GetActiveWindow(), "GlobalAlloc Failed!", "Error!", 0);
+        return 0;
+    }
+    blockdata = block;
+
+    //copy internal files to block
+    nameLength = (char*) data;
+    while (*nameLength > 0)
+    {
+        //name
+        i = (int) *nameLength;
+        nameLength ++;
+        message[0] = 0;
+        sprintf(message, "%d", i);
+        MessageBox(0, message, "", 0);
+        nameLength += i;
+        //data
+    }
+
+    //return block;
+    return 0;
+}
+
+int getSearchPaths(char *searchPath[], int count)
+{
+    //fills the searchPath array with paths to search for files
+    //returns 0 on failure > 0 on success
+    char *buffer;
+    DWORD reglen;
+    HKEY hKey;
+    long ret;
+    //find location of dbpro dir
+    ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Dark Basic\\Dark Basic Pro\\", 0, KEY_QUERY_VALUE, &hKey);
+    if (ret != ERROR_SUCCESS)
+    {
+        MessageBox(GetActiveWindow(), "DarkBASIC pro location not found", "Error!", 16);
+        return 0;
+    }
+    buffer = malloc(1024);
+    ret = RegQueryValueEx(hKey, "INSTALL-PATH", NULL, NULL, (LPBYTE) buffer, &reglen);
+    if (ret != ERROR_SUCCESS)
+    {
+        MessageBox(GetActiveWindow(), "DarkBASIC pro location not found", "Error!", 16);
+        return 0;
+    }
+    RegCloseKey(hKey);
+    //plugins
+    searchPath[0] = malloc(strlen(buffer)+21); //"\compiler\plugins\"
+    *searchPath[0] = 0x0;
+    strcat(searchPath[0], buffer);
+     strcat(searchPath[0], "\\compiler\\plugins\\");
+    //plugins-user
+    if (count > 1)
+    {
+        searchPath[1] = malloc(strlen(buffer)+25); //"\plugins-user\"
+        *searchPath[1] = 0x0;
+        strcat(searchPath[1], buffer);
+        strcat(searchPath[1], "\\compiler\\plugins-user\\");
+    }
+    //effects
+    if (count > 2)
+    {
+        searchPath[2] = malloc(strlen(buffer) + 20); //"\effects\"
+        *searchPath[2] = 0x0;
+        strcat(searchPath[2], buffer);
+        strcat(searchPath[2], "\\compiler\\effects\\");
+    }
+    free(buffer);
+    //add extra search paths here
+    return 1;
 }
 
 
