5ff6dedb981c32e86740843dc5b91f7b79a0e6f7
David <David@4a3fcead-100c-0410-8434-c0d6b4fee585>
2006-07-01 22:11:00 +0000

    Use windows crypto api to calculate md5 hash in patcher. This makes patcher a few Kb smaller.
    git-svn-id: http://crab/source/c#@111 4a3fcead-100c-0410-8434-c0d6b4fee585

--- patch_tool/trunk/run_gui/main.c
+++ patch_tool/trunk/run_gui/main.c
@@ -10,7 +10,7 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "md5.h"
+#include <Wincrypt.h>
 
 //functions
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -55,9 +55,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR CmdLine, i
     int x,y;
     HWND d_hwnd;
     RECT size;
-    
+
     char *name,*version,*buffer;
-    
+
     wndclass.style = 0;
     wndclass.lpfnWndProc = WndProc;
     wndclass.cbClsExtra = 0;
@@ -68,50 +68,50 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR CmdLine, i
     wndclass.hbrBackground = (HBRUSH) (COLOR_BTNFACE + 1);
     wndclass.lpszMenuName = NULL;
     wndclass.lpszClassName = AppName;
-    
+
     if(!RegisterClass(&wndclass))
     {
 		MessageBox(NULL, "Could not register window.", "Error!",MB_ICONEXCLAMATION | MB_OK);
 		return -1;
-	} 
-     
+	}
+
     //open patch file
     buffer = malloc(512);
     GetModuleFileName(NULL,buffer,512);
     patch = fopen(buffer,"rb");
     free(buffer);
-    
+
     //get length of patch tool
     fseek(patch, 0, SEEK_END);
     long patchLength = ftell(patch);
-    
+
     long dataStart = findHead(patch); //position in file patch data starts
     fseek(patch,dataStart,SEEK_SET);
-        
+
     //check there is actually data appended to patch
     if (dataStart >= patchLength)
     {
        MessageBox(NULL, "No patch data.", "Error!",MB_ICONEXCLAMATION | MB_OK);
 	   return -1;
     }
-    
+
     d_hwnd = GetDesktopWindow();
     GetWindowRect(d_hwnd,&size);
-    
+
     x = LOWORD(GetDialogBaseUnits());
     y = HIWORD(GetDialogBaseUnits());
-    
+
     x *= 50.5;
     y *= 27.5;
-    
+
     hwnd = CreateWindow(AppName,getName(patch), WS_CAPTION | WS_POPUPWINDOW | WS_MINIMIZEBOX, ((size.right-size.left)/2)-(x/2), ((size.bottom-size.top)/2)-(y/2), x, y, NULL, NULL, hInstance, NULL);
     ShowWindow(hwnd, CmdShow);
     UpdateWindow(hwnd);
-    
+
     hmenu = GetSystemMenu(hwnd, FALSE);
     AppendMenu(hmenu, MF_SEPARATOR, 0, NULL);
     AppendMenu(hmenu, MF_STRING, 1, "About");
-    
+
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
@@ -125,47 +125,47 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     static HWND btnCancel,btnBrowse,frame,label;
     char *buffer;
     int cx,x,cy,y;
-    
+
     switch (message)
     {
         case WM_CREATE:
-            
+
             cx = LOWORD(GetDialogBaseUnits());
             cy = HIWORD(GetDialogBaseUnits());
             x = cx ; y = cx;
-            
+
             //info textbox
             editInfo = CreateWindowEx(WS_EX_CLIENTEDGE, "edit", NULL, WS_CHILD | WS_BORDER | WS_VISIBLE | WS_HSCROLL | WS_VSCROLL | ES_READONLY | ES_LEFT | ES_MULTILINE | ES_AUTOHSCROLL | ES_AUTOVSCROLL,
                                     x,y,cx*48,cy*20, hwnd, (HMENU) 3, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
-            
+
             y += cy * 20.5;
-            
+
             //text label
             label = CreateWindow("static", "exe to patch", WS_CHILD | WS_VISIBLE, x, y, 12*cx, cy, hwnd, (HMENU)4, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
-            
+
             x += cx * 11;
-            
+
             //exe textbox
             editExe = CreateWindowEx(WS_EX_CLIENTEDGE, "edit", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_READONLY | ES_LEFT | ES_AUTOHSCROLL,
                                     x,y,cx*32,cy*1.5, hwnd, (HMENU) 5, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
-            
+
             x += cx * 33;
-            
+
             //exe browse button
             btnBrowse = CreateWindow("button", "...", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, x, y, cx*4, cy * 1.5, hwnd, (HMENU) 6, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
-            
+
             x = cx * 39;
             y += cy * 2;
-            
+
             //cancel button
             btnCancel = CreateWindow("button", "Exit", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, x, y, 10*cx, cy*2, hwnd, (HMENU) 2, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
-            
+
             x -= cx * 12;
-            
+
             //patch button
             btnPatch = CreateWindow("button", "Patch", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, x, y, 10*cx, cy*2, hwnd, (HMENU) 1, ((LPCREATESTRUCT) lParam)-> hInstance, NULL);
             EnableWindow(btnPatch, FALSE);
-            
+
             //filename
             filename = getName(patch);
             //checksum
@@ -181,13 +181,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
             //info
             SetWindowText(editInfo,getName(patch));
-            
+
             return 0;
-            
+
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
-            
+
         case WM_COMMAND:
             if (HIWORD(wParam) ==  BN_CLICKED)
             {
@@ -206,7 +206,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
                 }
             }
             return 0;
-            
+
         case WM_SYSCOMMAND:
             if (LOWORD(wParam) == 1)
             {
@@ -217,29 +217,55 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
+void getMd5(char* checksum, void* buffer, int bufferLen)
+{
+    //get md5 checksum string of buffer
+    HCRYPTPROV cryptProv;
+    HCRYPTHASH hash;
+    DWORD hashLen = 16;
+    int i;
+    unsigned char hashData[hashLen + 1];
+    if(CryptAcquireContext(&cryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+    {
+        CryptCreateHash(cryptProv, CALG_MD5, 0, 0, &hash);
+        CryptHashData(hash, buffer, bufferLen, 0);
+        CryptGetHashParam(hash, HP_HASHVAL, hashData, &hashLen, 0);
+        for (i = 0; i < 16; i++)
+        {
+            sprintf(checksum + i * 2, "%02x", hashData[i]);
+        }
+        checksum[32] = 0;
+        if(hash)
+            CryptDestroyHash(hash);
+        if(cryptProv)
+            CryptReleaseContext(cryptProv,0);
+    }
+    else
+    {
+        checksum[0] = 0;
+    }
+}
+
 void doPatch(HWND hwnd)
 {
     long head;
     char *buffer;
     int numItems,i;
-    
+
     char *oldname;     // name from oldexe
     char *oldnamepos;  // name from oldexe
     int action;        // was an action done?
     int oldlen;        // lenght from oldexe
     int oldlendata;    // lenght from oldexe for filedata
-    
+
     //md5 checksum stuff
-    md5_state_t state;
-	md5_byte_t digest[16];
-	int di;
-	char hex_output[16*2 + 1]; //generated hash
-    
+	char hex_output[32 + 1]; //generated hash
+
     //disable patch button
     EnableWindow(btnPatch, FALSE);
-    
+
     //check checksum
-    if (checksum != 0x0)
+    if (checksum != NULL)
     {
         f_oldexe = fopen(proexe,"rb");
         fseek(f_oldexe,0,SEEK_END);
@@ -248,14 +274,9 @@ void doPatch(HWND hwnd)
         fseek(f_oldexe,0,SEEK_SET);
         fread(buffer,1,oldlen,f_oldexe);
         fclose(f_oldexe);
-        md5_init(&state);
-       	md5_append(&state, (const md5_byte_t *)buffer, oldlen);
-        md5_finish(&state, digest);
+        getMd5(hex_output, buffer, oldlen);
+        MessageBox(GetActiveWindow(), hex_output, "", 0);
         free(buffer);
-        for (di = 0; di < 16; ++di)
-        {
-            sprintf(hex_output + di * 2, "%02x", digest[di]);
-        }
         //check checksums match
         if (strcmp(hex_output,checksum) != 0)
         {
@@ -263,7 +284,7 @@ void doPatch(HWND hwnd)
             return;
         }
     }
-    
+
     //rename proexe to proexe + ".old"
     oldexe = malloc(strlen(proexe)+4);
     *oldexe = 0x0;
@@ -271,21 +292,21 @@ void doPatch(HWND hwnd)
     strcat(oldexe,".old");
     DeleteFile(oldexe);
     MoveFile(proexe,oldexe);
-    
+
     //open old exe and new exe
     f_oldexe = fopen(oldexe,"rb");
     f_newexe = fopen(proexe,"wb");
-    
+
     //find head of f_oldexe and write it to f_newexe
     head = findHead(f_oldexe);
-    
+
     //write head to newexe
     rewind(f_oldexe);
     buffer = malloc(head);
     fread(buffer,1,head,f_oldexe);
     fwrite(buffer,1,head,f_newexe);
     free(buffer);
-    
+
     //get a list of files in patch
     numItems = getByte(patch);
     items Items[numItems+1];
@@ -303,7 +324,7 @@ void doPatch(HWND hwnd)
             fseek(patch,Items[i].length,SEEK_CUR);
         }
     }
-    
+
     //for each file in proexe check if there is a file in the patch
     oldlen = 1;
     while (oldlen > 0 && oldlen < 100)
@@ -348,7 +369,7 @@ void doPatch(HWND hwnd)
                         fread(&oldlendata,1,4,f_oldexe);
                         fseek(f_oldexe,oldlendata,SEEK_CUR);
                         break;
-                        
+
                         case 1 :
                         // remove
                         //skip date in oldexe
@@ -403,7 +424,7 @@ void doPatch(HWND hwnd)
                         fwrite(buffer,1,Items[i].length,f_newexe);
                         free(buffer);
                         break;
-                        
+
                         case 1 :
                         //remove
                         buffer = malloc(23+strlen(Items[i].name));
@@ -426,12 +447,12 @@ void doPatch(HWND hwnd)
             }
         }
     }
-    
+
     //clean up
     fclose(f_oldexe);
     fclose(f_newexe);
     fclose(patch);
-    
+
     //Success Message
     MessageBox(hwnd,"Patch Success!","",64);
 }
@@ -500,7 +521,7 @@ void getProExe(HWND hwnd)
       {
             *filterpos = *filepos;
             filterpos ++;
-            filepos ++;  
+            filepos ++;
       }
       *filterpos = 0x0;
       filterpos ++;
@@ -509,12 +530,12 @@ void getProExe(HWND hwnd)
       {
             *filterpos = *filepos;
             filterpos ++;
-            filepos ++;  
+            filepos ++;
       }
       *filterpos = 0x0;
       filterpos ++;
       *filterpos = 0x0;
-      
+
       OPENFILENAME ofn;
       ZeroMemory(&ofn, sizeof(OPENFILENAME));
       ofn.lStructSize = sizeof(ofn);
